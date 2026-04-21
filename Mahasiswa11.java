b5bebf3201c625318c9e09b966a32d7e960a2745
firdauzynuzula <nuzulafirdhauzy@gmail.com>
2026-04-21 22:03:31 +0700

    update file Mahasiswa11.java

--- Mahasiswa11.java
+++ Mahasiswa11.java
@@ -1,3 +1,19 @@
 public class Mahasiswa11 {
+    String nim;
+    String nama;
+    String kelas;
+    int nilai;
+
+    Mahasiswa11() {}
+
+    Mahasiswa11(String nim, String nama, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.kelas = kelas;
+        nilai = -1;
+    }
     
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
 }
