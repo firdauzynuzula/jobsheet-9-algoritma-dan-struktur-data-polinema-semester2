7aea8900615cc9aa5eeb4e9bba5aea83c8de9c2f
firdauzynuzula <nuzulafirdhauzy@gmail.com>
2026-04-22 06:19:49 +0700

    melengkapi method stack pada StackTugasMahasiswa11.java

--- StackTugasMahasiswa11.java
+++ StackTugasMahasiswa11.java
@@ -1,4 +1,66 @@
 public class StackTugasMahasiswa11 {
 
+    Mahasiswa11[] stack;
+    int top;
+    int size;
+
+    public StackTugasMahasiswa11(int size) {
+        this.size = size;
+        stack = new Mahasiswa11[size];
+        top = -1;
+    }
+
+    public boolean isFull() {
+        if (top == size - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void push(Mahasiswa11 mhs) {
+        if (!isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack penuh! Tidak bisa menambahkan tugas lagi.");
+        }
+    }
     
+
+    public Mahasiswa11 pop() {
+        if (!isEmpty()) {
+            Mahasiswa11 m = stack[top];
+            top--;
+            return m;
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas untuk dinilai.");
+            return null;
+        }
+    }
+
+    public Mahasiswa11 peek() {
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas untuk dikumpulkan.");
+            return null;
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i <= top; i++) {
+            System.out.println("NIM: " + stack[i].nim + ", Nama: " + stack[i].nama + ", Kelas: " + stack[i].kelas
+                    + ", Nilai: " + stack[i].nilai);
+        }
+        System.out.println("");
+    }
 }
