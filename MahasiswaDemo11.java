bdaf4f2566c4e11bf3c159fc4d319959d788f078
firdauzynuzula <nuzulafirdhauzy@gmail.com>
2026-04-22 07:29:39 +0700

    push change

--- MahasiswaDemo11.java
+++ MahasiswaDemo11.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;   
+
 public class MahasiswaDemo11 {
-    
+    public static void main(String[] args) {
+        StackTugasMahasiswa11 stack = new StackTugasMahasiswa11(5);
+        Scanner scan = new Scanner(System.in);
+
+        
+      
+    }
 }
